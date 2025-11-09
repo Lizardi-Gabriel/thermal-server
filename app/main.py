503fdda3a1a7cf87cbe1daeb94b104b55668c116
LizardiGabriel <62402702+Lizardi-Gabriel@users.noreply.github.com>
2025-11-09 12:27:59 -0600

    tiempo de respuesta

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.routers import router as api_router
 from app.gallery import router as gallery_router
 from app.publicEndpoints import router as public_router
 from app.routers_optimizado import router as optimizado_router
 from app.routers_admin import router as admin_router
+import time
 
 
 
@@ -13,6 +14,18 @@ app = FastAPI(
     description="API para monitorear calidad del aire y gestionar usuarios e imágenes",
     version="1.0.0"
 )
+
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start
+    print(f"Tiempo de respuesta: {process_time:.4f} segundos: {request.url.path}")
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
 
 # Registrar los routers
 app.include_router(api_router)
